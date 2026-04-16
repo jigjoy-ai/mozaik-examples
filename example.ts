9febc8df23b160fdeb3fd3d100f39d98a5149c41
Mijura <miodragvilotijevic@gmail.com>
2026-04-16 17:10:29 +0200

    try tool calling

--- example.ts
+++ example.ts
@@ -3,6 +3,7 @@ import { UserMessage, DeveloperMessage } from "@mozaik-ai/core"
 import { InMemoryContextRepository } from "./in-memory-context-repository"
 import "dotenv/config"
 import { InferenceRequest } from "@mozaik-ai/core"
+import { Tool } from "@mozaik-ai/core"
 
 async function main() {
 	const message = UserMessage.create("Tell me a joke about birds")
@@ -20,6 +21,26 @@ async function main() {
 
 	const model = new Gpt54()
 	model.setReasoningEffort("medium")
+
+	const tool: Tool = {
+		name: "get_bird_type",
+		description: "Get the type of bird for a joke",
+		type: "function",
+		parameters: {
+			type: "object",
+			properties: {
+				bird_type: { type: "string" },
+			},
+			required: ["bird_type"],
+		},
+		strict: true,
+		invoke: async () => {
+			return {
+				bird_type: "pigeon",
+			}
+		},
+	}
+	model.setTools([tool])
 	const request = new InferenceRequest(model, context)
 	const response = await openAiResponses.infer(request)
 	const newContextItems = response.contextItems
@@ -27,7 +48,8 @@ async function main() {
 
 	await contextRepository.save(context)
 	const restoredContexts = await contextRepository.getByProjectId(projectId)
-	console.log(restoredContexts)
+	console.log(JSON.stringify(restoredContexts, null, 2))
+	console.log(response.tokenUsage)
 }
 
 main().catch((error) => {
