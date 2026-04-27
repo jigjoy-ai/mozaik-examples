71ac6f62c53018e3f77c6415070b015043552f90
Mijura <miodragvilotijevic@gmail.com>
2026-04-27 14:27:30 +0200

    extend example

--- standup-comedy/standup.ts
+++ standup-comedy/standup.ts
@@ -1,4 +1,4 @@
-import { AgentRuntime, ModelContext, DeveloperMessage, Gpt54, UserMessage, AgentSociety } from "@mozaik-ai/core"
+import { AgentRuntime, ModelContext, DeveloperMessage, Gpt54, UserMessage, AgentSociety, ModelMessage } from "@mozaik-ai/core"
 import { getBirdTypeTool } from "../utils/tools/get-bird-type"
 import { JokeTellerAgent } from "./joke-teller"
 import { TimeCounter } from "./time-counter"
@@ -8,14 +8,15 @@ const agentRuntime = new AgentRuntime()
 const agent = new JokeTellerAgent(agentRuntime)
 
 agent.setInferenceVisitor(new TimeCounter())
-const messageText = "Tell me a joke about birds"
+const outputMessage = ModelMessage.rehydrate({ text: "Why did the pigeon quit his job? Because he was tired of working for breadcrumbs." })
+const messageText = "Tell me a joke about cars"
 const message = UserMessage.create(messageText)
 const developerMessage = DeveloperMessage.create(
 	"You are a joke teller. You will be given a joke and you will need to tell it to the user.",
 )
 
 const projectId = `pr-${crypto.randomUUID()}`
-const context = ModelContext.create(projectId).addContextItem(developerMessage).addContextItem(message)
+const context = ModelContext.create(projectId).addContextItem(outputMessage).addContextItem(developerMessage).addContextItem(message)
 
 const model = new Gpt54()
 model.setReasoningEffort("high")
